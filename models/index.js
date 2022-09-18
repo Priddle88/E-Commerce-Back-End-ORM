5a13b58175eac48c1e18f79750943d2fa1771ba9
Parker <parkerriddle09@gmail.com>
2022-09-17 22:18:28 -0600

    I moved the wrong files from the orignal folder. Just fixed it

--- models/index.js
+++ models/index.js
@@ -1,23 +1,20 @@
-const Reader = require('./Travellers');
-const Book = require('./Locations');
-const LibraryCard = require('./Trips');
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
 
-Reader.hasOne(LibraryCard, {
-  foreignKey: 'reader_id',
-  onDelete: 'CASCADE',
-});
+// Products belongsTo Category
 
-Reader.hasMany(Book, {
-  foreignKey: 'reader_id',
-  onDelete: 'CASCADE',
-});
+// Categories have many Products
 
-Book.belongsTo(Reader, {
-  foreignKey: 'reader_id',
-});
+// Products belongToMany Tags (through ProductTag)
 
-LibraryCard.belongsTo(Reader, {
-  foreignKey: 'reader_id',
-});
+// Tags belongToMany Products (through ProductTag)
 
-module.exports = { Reader, Book, LibraryCard };
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
