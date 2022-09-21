168ac98fca8b80488948a879eb4845509b1d5d3e
Parker <parkerriddle09@gmail.com>
2022-09-21 00:36:51 -0600

    Product routes ggod to go. Just finished up with the delete route for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
+const { sync } = require('../../models/Tag');
 
 // The `/api/products` endpoint
 
@@ -112,8 +113,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const categoryData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
